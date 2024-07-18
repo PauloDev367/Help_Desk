dea305b6b72de539b6f1a80628336302664f29d4
Paulo <pauloecomercial367@gmail.com>
2024-07-18 08:20:11 -0300

    :sparkles: Comment rules

--- HelpDeskService/Core/Domain/Entities/Comment.cs
+++ HelpDeskService/Core/Domain/Entities/Comment.cs
@@ -4,9 +4,38 @@ public class Comment
     public Guid Id { get; set; }
     public string Text { get; set; }
     public bool IsClientComment { get; set; }
-    //verificar se caso o client não existir, informar o support, o msm para o contrario
-    public Guid? SupportId { get; set; }
+    private Guid? supportId;
+    private Guid? clientId;
+    public Guid? SupportId
+    {
+        get => supportId;
+        set
+        {
+            supportId = value;
+            Validate();
+        }
+    }
     public Support? Support { get; set; }
+    public Guid? ClientId
+    {
+        get => clientId;
+        set
+        {
+            clientId = value;
+            Validate();
+        }
+    }
     public Client? Client { get; set; }
-    public Guid? ClientId { get; set; }
+    public void Validate()
+    {
+        if (IsClientComment && (ClientId == null || Client == null))
+        {
+            throw new InvalidOperationException("Client must be specified for client comments.");
+        }
+
+        if (!IsClientComment && (SupportId == null || Support == null))
+        {
+            throw new InvalidOperationException("Support must be specified for support comments.");
+        }
+    }
 }
