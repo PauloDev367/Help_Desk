924d59cae5afb30a05b6aa7521f6dcc5e8aa587b
Paulo <pauloecomercial367@gmail.com>
2024-07-21 19:09:44 -0300

    :sparkles: Login route

---
+++ HelpDeskService/Consumers/Api/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Api.ViewModels;
+using Application.Auth.Request;
+using Domain.Ports;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/v1/login")]
+public class AuthController : ControllerBase
+{
+
+    private readonly IAuthUserService _authUserService;
+
+    public AuthController(IAuthUserService authUserService)
+    {
+        _authUserService = authUserService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> LoginAsync([FromBody] LoginUserVM vm)
+    {
+        var loginUserRequest = new LoginUserRequest
+        {
+            Email = vm.Email,
+            Password = vm.Password,
+        };
+
+        var auth = await _authUserService.AuthenticateAsync(loginUserRequest);
+        if (auth.Errors.Count > 0)
+        {
+            return BadRequest(auth);
+        }
+        return Ok(auth);
+    }
+
+}
---
+++ HelpDeskService/Consumers/Api/ViewModels/LoginUserVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.ViewModels;
+
+public record LoginUserVM
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
